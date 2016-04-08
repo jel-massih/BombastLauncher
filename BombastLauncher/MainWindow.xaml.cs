965292e4f23e3dc594a6e1dbee00d63972fefea1
Jason El-Massih <jel-massih@hotmail.com>
2016-04-08 00:20:20 -0400

    Initial MainWindow Setup

--- BombastLauncher/MainWindow.xaml.cs
+++ BombastLauncher/MainWindow.xaml.cs
@@ -15,14 +15,55 @@ using System.Windows.Shapes;
 
 namespace BombastLauncher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool m_restoreForDragMove;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void titleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2)
+            {
+                if(WindowState == WindowState.Normal)
+                {
+                    WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    WindowState = WindowState.Normal;
+                }
+            }
+            else
+            {
+                m_restoreForDragMove = WindowState == WindowState.Maximized;
+                DragMove();
+            }
+        }
+
+        private void titleBar_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            m_restoreForDragMove = false;
+        }
+
+        private void titleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(m_restoreForDragMove)
+            {
+                m_restoreForDragMove = false;
+
+                var point = PointToScreen(e.MouseDevice.GetPosition(this));
+
+                Left = point.X - (RestoreBounds.Width * 0.5);
+                Top = point.Y;
+
+                WindowState = WindowState.Normal;
+                DragMove();
+            }
+        }
+
     }
 }
